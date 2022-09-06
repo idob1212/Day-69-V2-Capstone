26b1b9834a79de470f87121219e116c1998c9f2c
Ido Beerie <idobeerie46@gmail.com>
2022-09-06 16:32:36 +0300

    Add gunicorn

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ from wtforms.validators import DataRequired
 from flask_login import LoginManager
 from functools import wraps
 from sqlalchemy import ForeignKey
+import os
+
 
 login_manager = LoginManager()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager.init_app(app)
